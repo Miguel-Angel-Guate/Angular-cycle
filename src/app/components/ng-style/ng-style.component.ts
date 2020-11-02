be8657625421de6c771a93bdfaf9632b86c38f4e
Miguel Angel <miguel.teletor@atmira.com>
2020-11-02 16:25:28 +0100

    feat: add style

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  templateUrl: './ng-style.component.html',
+  styleUrls: ['./ng-style.component.css'],
+})
+export class NgStyleComponent implements OnInit {
+  tamano: number = 40;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
